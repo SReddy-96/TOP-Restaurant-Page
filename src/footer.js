961a0c2b8e85f6a431d4f51c8aa74a998fd4e9b4
Steven Reddy <steveng.reddy@gmail.com>
2024-06-23 12:26:08 +0900

    Initial dist subtree commit

--- src/footer.js
+++
@@ -1,8 +0,0 @@
-export default function footer(){
-    // footer
-    const footerWrapper = document.querySelector('.footer');
-    const footerText = document.createElement('p');
-    footerText.innerHTML = "Nonna's Pizza &#169; 2024 by Steven Reddy ";
-    
-    footerWrapper.append(footerText);
-}
